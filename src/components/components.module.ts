4c87884f68de5884ab3d64369349b312b6ed7a2e
Alessandro Barbosa <alessandro.barbosa@conatussw.com>
2018-06-20 17:07:19 -0400

    Exclusão de projeto TS

--- src/components/components.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { GiuliaviewComponent } from './giuliaview/giuliaview';
-@NgModule({
-	declarations: [GiuliaviewComponent],
-	imports: [],
-	exports: [GiuliaviewComponent]
-})
-export class ComponentsModule {}
